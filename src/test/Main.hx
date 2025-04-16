4f7c69644abfe154036816a86e25ec1651139b41
Asher Hoeveler <ahoeveler@apotechgroup.com>
2025-04-15 19:12:56 -0500

    Test

--- src/test/Main.hx
+++ src/test/Main.hx
@@ -1,7 +1,13 @@
 package test;
 
+import plotly.Plotly;
+import js.Browser;
+
 class Main {
     static function main() {
-        
+        final targetDiv = Browser.document.createDivElement();
+        targetDiv.style.width = "100vw";
+        targetDiv.style.height = "100vh";
+        final plot = new Plotly(targetDiv);
     }
 }
