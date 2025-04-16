773d1b5b962fc5e49ac71f061031e06066c984ab
Asher Hoeveler <ahoeveler@apotechgroup.com>
2025-04-15 19:11:09 -0500

    Greater specificity on accepted inputs

--- src/plotly/Plotly.hx
+++ src/plotly/Plotly.hx
@@ -1,13 +1,17 @@
 package plotly;
 
+import plotly.ext.figuretypes.Enums.PlotType;
+import js.html.DivElement;
 import plotly.ext.Plotly.FrameDataT;
 import js.html.Node;
 import haxe.extern.EitherType;
-private typedef ID = String;
+
+
+private typedef GraphDivRef = EitherType<String, DivElement>;
 class Plotly {
     var plotElement:Node;
     var data:FrameDataT;
-    public function new(fromElement:EitherType<ID, Node>) {
+    public function new(fromElement:GraphDivRef) {
         plotElement = plotly.ext.Plotly.newPlot(fromElement, data);
     }
     public function update() {
