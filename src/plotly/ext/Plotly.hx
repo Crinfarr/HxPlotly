fb705de864914d1c273ad83565e5f95a362dfe93
Asher Hoeveler <ahoeveler@apotechgroup.com>
2025-04-15 19:11:32 -0500

    Correct class name

--- src/plotly/ext/Plotly.hx
+++ src/plotly/ext/Plotly.hx
@@ -10,6 +10,7 @@ typedef FrameDataT = {
 	frames:Array<Any>
 }
 
+@:native("Plotly")
 extern class Plotly {	
     overload public static extern function newPlot(node:EitherType<Node, String>, data:Array<Any>, layout:{}, config:{}):Node;
     overload public static extern function newPlot(node:EitherType<Node, String>, obj:FrameDataT):Node;
